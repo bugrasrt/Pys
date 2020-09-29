d39c996236f9e4b674d979d89fa4b714c8e55a82
Bugra <bugrasrt55@gmail.com>
2020-09-29 17:32:20 +0300

    Update word_List.py

--- word_List.py
+++ word_List.py
@@ -1,10 +1,12 @@
 from itertools import permutations
 
 # Enter several word or letter then it will create unique words then sort it.
-while True:
-    words = input('Enter the words(For exit, enter 0): ')
+# You can write it to your txt file
+
+def word_create():
+    words = input('Enter the words(Exit 0): ')
     if words == '0':
-        break
+        exit(1)
     else:
         char_List = [char if char != ' ' else '' for char in words]
 
@@ -15,17 +17,34 @@ while True:
         word += ''.join(xs)
         word_List.add(word)
         word = ''
+        
+    return word_List
 
-    def order(input_List):
-        sorted_List = list(input_List)
-        sorted_List.sort()
-        for word in sorted_List:
-          print(word)
+def list_order(input_List):
+    sorted_List = list(input_List)
+    sorted_List.sort()
+    for word in sorted_List:
+        print(word)
+            
+    return sorted_List
 
-    order(word_List)
+def file_create(input_List):
+    file = open(r'C:/Users/Bugra/Desktop/my_word_List.txt', 'a+')
+    file.write('####New List####\n')
+    for word in input_List:
+          file.write(f'{word}\n')
+    file.write('\n')
+    file.close()
+
+
+while True:
+    words = word_create()
+    sorted_List = list_order(words)
     
-    exit = input('For exit enter 0, otherwise press any key: ')
-    if exit == '0':
+    choose = input('Exit(0) or Write_to_File(1), otherwise press any key: ')
+    if choose == '0':
         break
+    elif choose == '1':
+        file_create(sorted_List)
     else:
         pass
